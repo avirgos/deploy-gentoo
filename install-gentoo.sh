f92823dd531df0c738f1423b012c9e6eb4ec5d4d
Antoine Virgos <antoine@virgos.fr>
2024-01-30 11:24:09 +0100

    Initial commit

---
+++ install-gentoo.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+######################################################################
+# Template
+######################################################################
+set -o errexit  # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset  # Exit if variable not set.
+IFS=$'\n\t'     # Remove the initial space and instead use '\n'.
+
+######################################################################
+# Global variables
+######################################################################
+USERNAME="<username>"
+LOCAL_IP="<local-ip>"
+LOCATION_REPO="<location-repo>"
+
+STAGE3_FILE="stage3-amd64-openrc-20240121T170320Z.tar.xz"  
+PORTAGE_FILE="portage-latest.tar.bz2"                       
+DISTFILES_FILE="distfiles.tar.bz2"                          
+PACKAGES_FILE="packages.tar.bz2"                           
+
+######################################################################
+# Partitionnement
+######################################################################
+cfdisk /dev/sda
+
+######################################################################
+# Système de fichiers
+######################################################################
+mkfs.ext3 /dev/sda1  # /boot
+mkfs.ext3 /dev/sda2  # /
+mkfs.ext3 /dev/sda5  # /home
+mkfs.ext3 /dev/sda6  # /var
+mkswap /dev/sda7     # swap
+
+######################################################################
+# Montage du système de fichiers 
+######################################################################
+mount /dev/sda2 /mnt/gentoo
+mkdir /mnt/gentoo/boot
+mount /dev/sda1 /mnt/gentoo/boot
+mkdir /mnt/gentoo/home
+mount /dev/sda5 /mnt/gentoo/home
+mkdir /mnt/gentoo/var
+mount /dev/sda6 /mnt/gentoo/var
+swapon /dev/sda7
+
+######################################################################
+# fstab
+######################################################################
+echo "/dev/sda1      /boot       ext2    noauto,noatime       0 2" >> /mnt/gentoo/etc/fstab
+echo "/dev/sda2      /           ext3    noatime              0 1" >> /mnt/gentoo/etc/fstab
+echo "/dev/sda5      /home       ext3    noatime              0 2" >> /mnt/gentoo/etc/fstab
+echo "/dev/sda6      /var        ext3    noatime              0 2" >> /mnt/gentoo/etc/fstab
+echo "/dev/sda7      none        swap    sw                   0 0" >> /mnt/gentoo/etc/fstab
+
+######################################################################
+# Paquets
+######################################################################
+##########
+# stage3 #
+##########
+rsync -avP "${USERNAME}"@"${LOCAL_IP}":"${LOCATION_REPO}"/deploy-gentoo/"${STAGE3_FILE}" /mnt/gentoo/
+cd /mnt/gentoo
+tar xJf stage3*
+
+###########
+# portage #
+###########
+rsync -avP "${USERNAME}"@"${LOCAL_IP}":"${LOCATION_REPO}"/deploy-gentoo/"${PORTAGE_FILE}" /mnt/gentoo/usr/
+cd /mnt/gentoo/usr
+tar xjf "${PORTAGE_FILE}"
+
+#############
+# distfiles #
+#############
+rsync -avP "${USERNAME}"@"${LOCAL_IP}":"${LOCATION_REPO}"/deploy-gentoo/"${DISTFILES_FILE}" /mnt/gentoo/var/cache/
+cd /mnt/gentoo/var/cache
+tar xjf "${DISTFILES_FILE}"
+
+############
+# packages #
+############
+rsync -avP "${USERNAME}"@"${LOCAL_IP}":"${LOCATION_REPO}"/deploy-gentoo/"${PACKAGES_FILE}" /mnt/gentoo/var/cache/
+tar xjf "${PACKAGES_FILE}"
+
+######################################################################
+# chroot
+######################################################################
+cd /
+mount -t proc none /mnt/gentoo/proc
+mount -o bind /dev /mnt/gentoo/dev
+cp -L /etc/resolv.conf /mnt/gentoo/etc/
+chroot /mnt/gentoo /bin/bash <<'EOF'
+env-update && source /etc/profile
+
+#    #################################################################
+# -> # portage 
+#    #################################################################
+env-update
+ln -s /usr/portage /var/db/repos/gentoo
+env-update
+
+######################################################################
+# Noyau
+######################################################################
+emerge -K gentoo-kernel-bin
+
+######################################################################
+# cronie
+######################################################################
+emerge -K cronie
+rc-update add cronie default
+
+######################################################################
+# dhcpcd
+######################################################################
+emerge -K dhcpcd
+rc-update add dhcpcd default
+
+######################################################################
+# GRUB
+######################################################################
+emerge -K grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+######################################################################
+# Mot de passe
+######################################################################
+echo "root:Attach3player3darken" | chpasswd
+
+######################################################################
+# Heure
+######################################################################
+echo 'TIMEZONE="Europe/Paris"' > /etc/conf.d/clock
+
+######################################################################
+# Clavier
+######################################################################
+sed -i 's/keymap="us"/keymap="fr"/' /etc/conf.d/keymaps
+
+exit
+
+EOF
+
+######################################################################
+# Redémarrage 
+######################################################################
+reboot
---
+++ manage-gentoo.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+######################################################################
+# Template
+######################################################################
+set -o errexit  # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset  # Exit if variable not set.
+IFS=$'\n\t'     # Remove the initial space and instead use '\n'.
+
+######################################################################
+# Global variables
+######################################################################
+VG_NAME="VG1"
+NEW_HOSTNAME="<hostname>"
+
+######################################################################
+# Hostname
+######################################################################
+echo "hostname=\"${NEW_HOSTNAME}\"" > /etc/conf.d/hostname
+hostname "${NEW_HOSTNAME}"
+sed -i "s/localhost/${NEW_HOSTNAME}/g" /etc/hosts
+
+######################################################################
+# SSH
+######################################################################
+rc-update add sshd default
+/etc/init.d/sshd start
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+rc-service sshd restart
+
+######################################################################
+# Utilisateur
+######################################################################
+useradd -m -G users,wheel exam1
+echo "exam1:Attach4player4darken" | chpasswd
+
+######################################################################
+# cronie
+######################################################################
+emerge -K cronie
+rc-update add cronie default
+
+######################################################################
+# syslog-ng
+######################################################################
+emerge -K syslog-ng
+rc-update add syslog-ng default
+
+echo "
+destination d_login {
+    file("/var/log/login.log");
+};
+
+filter f_login {
+    program("login");
+};
+
+log {
+    source(src);
+    filter(f_login);
+    destination(d_login);
+};
+" | tee -a /etc/syslog-ng/syslog-ng.conf
+rc-service syslog-ng restart
+
+######################################################################
+# cron 
+######################################################################
+mkdir -p /home/toto/data
+mkdir -p /mnt/backup
+
+echo "
+#!/bin/bash
+
+source_dir=\"/home/toto/data\"
+backup_dir=\"/mnt/backup\"
+timestamp=\$(date +%Y%m%d%H%M%S)
+
+# Vérifier si le répertoire source existe
+if [ -d \"\$source_dir\" ]; then
+    # Créer une archive tar du répertoire source
+    tar -czf \"\$backup_dir/backup_\$timestamp.tar.gz\" -C \"\$source_dir\" .
+    echo \"Sauvegarde effectuée avec succès.\"
+else
+    echo \"Erreur : Le répertoire source n'existe pas.\"
+    exit 1
+fi
+" > /etc/cron.d/backup.sh
+chmod u+x /etc/cron.d/backup.sh
+
+####################################################
+# cron.d : lancer le script tous les jours à 15h20 #
+####################################################
+echo "20 15 * * * root /etc/cron.d/backup.sh" > /etc/cron.d/backup
+
+/etc/init.d/cronie restart
+
+######################################################################
+# apache 
+######################################################################
+emerge -K www-servers/apache
+rc-update add apache2 default
+
+mkdir -p /etc/ssl/apache2
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/apache2/server.key -out /etc/ssl/apache2/server.crt
+
+echo "ServerName localhost" >> /etc/apache2/httpd.conf
+
+/etc/init.d/apache2 start
+
+######################################################################
+# proftpd
+######################################################################
+emerge -K net-ftp/proftpd
+rc-update add proftpd default
+
+cp /etc/proftpd/proftpd.conf.sample /etc/proftpd/proftpd.conf
+
+/etc/init.d/proftpd start
+
+######################################################################
+# RAID 
+######################################################################
+emerge sys-fs/mdadm
+
+modprobe raid1
+mknod /dev/md1 b 9 1
+mdadm --create --metadata=0.9 /dev/md1 --level=1 --raid-devices=2 /dev/sda4 missing
+
+cfdisk /dev/md1         # md1p1
+mkfs.ext3 /dev/md1p1
+
+mkdir -p /mnt/backup
+echo "/dev/md1p1 /mnt/backup ext3 defaults 0 2" >> /etc/fstab
+mount /mnt/backup
